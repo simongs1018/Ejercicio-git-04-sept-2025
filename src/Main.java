895aed595b10bfe9c8914a80991b024aab098c5d
simongs1018 <simongomez@ucundinamarca.edu.co>
2025-09-05 17:57:36 -0500

    Ejercicios 5-09

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+                System.out.println("comandos git");
+                System.out.println("Git status: para estado de las ramas\n");
+                System.out.println("Guy brach: para ver las ramas\n");
+                System.out.println("Git add: para agregar archivos");
+                System.out.println("Git push origin: para subir cambios ");
+                System.out.println("Git comit -m: para guardar cambios local\n");
+                System.out.println("Git config --list: listar configuración \n");
+                System.out.println("Git log: ver todos los comit");
+                System.out.println("Git clone : para clonar \n");
+                System.out.println("Git fetch : ver ramas remotas\n");
+                System.out.println("Git switch : para crear y posicionarnos sobre la rama");
+                System.out.println("Git comit -m \" resumen comandos git");
+                System.out.println("Git push origin RamaDesarrollo");
+
+
+        }
+    }
